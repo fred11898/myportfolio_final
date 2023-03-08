a7c503224ceafa469c1c84eb9362a99d13c6e51f
fred11898 <fredjonesvillapa@gmail.com>
2023-03-08 23:38:17 +0800

    My New Portfolio, i guess i will make it my final

---
+++ style/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typed", {
+    strings: ["Web Developer", "Web Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop:true
+  })
+  
+  console.log(typed)
